7603140bc2b3a7805d060b86b2a42bb9235ca2b3
Fabiano Melo <svarssel@gmail.com>
2021-03-14 01:22:17 -0300

    crud_dao

---
+++ crud_dao.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `usuarios` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) DEFAULT NULL,
+  `email` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+
